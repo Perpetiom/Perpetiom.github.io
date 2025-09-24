abb4c57b8397264cc752adbc0b9bb45a4cdb25c8
Legondr <pansufurki@gmail.com>
2025-09-24 16:53:51 +0200

    build attempt #9

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const base = '/perpetiom';
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
     trailingSlash: true,
-    basePath: base,
-    assetPrefix: base + '/',
 
     // ať build nepada kvůli ESLintu/TS
     eslint: { ignoreDuringBuilds: true },
